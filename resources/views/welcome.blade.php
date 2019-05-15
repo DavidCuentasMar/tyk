2d4c651bf92757a56fc54ea9fefd0cd022f04f3a
WilliamCadenas21 <williamcadenas21@gmail.com>
2019-05-15 11:05:25 -0500

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,50 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+@section('content')
+<div class="jumbotron">
+    <h1 class="display-4">Bienvenido a Tuyuka!</h1>
+    <p class="lead">Tuyuka es la web de comercio para conectar campesinos con grandes compradores </p>
+    <hr class="my-4">
+    <p>¿ Quieres empezar ya ? registrate y empieza a vender o a comprar a través de nuestra web.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Registrate!</a>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="https://www.uccs.mx/images/library/image/argentina/2018/campesinos.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Compromiso</h5>
+                    <p class="card-text">En Tuyuka estamos comprometidos con el campesino a proporcionar conexion y que venda todos sus productos</p>
+                    <a href="#" class="btn btn-primary">Conocenos</a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="https://farm5.static.flickr.com/4016/4639061927_0363a38865.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Facilidad</h5>
+                    <p class="card-text">Montar los productos que deseas vender es super facil y rapido.</p>
+                    <a href="#" class="btn btn-primary">Registrate</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="https://www.movemosalmundo.com/wp-content/uploads/2018/03/alianza-campesinos-750x356.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Maxima visibilidad</h5>
+                    <p class="card-text">Tus productos son igual de visibles que cualquiera en la plataforma, tan solo cobramos pequeñas comisiones por el monto de la venta realizada</p>
+                    <a href="#" class="btn btn-primary">Conoce nuestros terminos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+
+
+@endsection('content')
